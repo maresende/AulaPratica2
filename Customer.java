7d49576e12ebaaa62a4a1dd18c445c96149440f5
matheus666matheus <matheus.bh2@hotmail.com>
2019-05-26 19:45:43 -0300

    replace temp with jquery

--- Customer.java
+++ Customer.java
@@ -18,29 +18,22 @@ class Customer {
    private double amountFor(Rental aRental) {
       return aRental.getCharge();  // agora apenas delega chamada para método movido
    }
-	
 
    public String statement() {
-      double totalAmount = 0;
-      int frequentRenterPoints = 0;
-      Enumeration rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         
-         frequentRenterPoints += each.getFrequentRenterPoints();
+   Enumeration rentals = _rentals.elements();
+   String result = "Rental Record for " + getName() + "\n";
+   while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
 
-         // show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
+      // show figures for this rental
+      result += "\t" + each.getMovie().getTitle()+ "\t" +
                 String.valueOf(each.getCharge()) + "\n";
-         totalAmount += each.getCharge();
-      }
+   }
 
-      // add footer lines
-      result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-      result += "You earned " + String.valueOf(frequentRenterPoints) +
-             " frequent renter points";
-      return result;
+   // add footer lines
+   result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+   result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
+                   " frequent renter points";
+   return result;
   }
-
 }
