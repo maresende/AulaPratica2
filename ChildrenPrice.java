727ec9dd14f53670ea5ec95f88705dd2784fe28f
matheus666matheus <matheus.bh2@hotmail.com>
2019-05-26 20:11:15 -0300

    edit final

---
+++ ChildrenPrice.java
@@ -0,0 +1,11 @@
+class ChildrensPrice extends Price {
+    double getCharge(int daysRented) {
+       double result = 1.5;
+       if (daysRented > 3)
+          result += (daysRented - 3) * 1.5;
+       return result;
+     }
+   int getPriceCode() {
+       return Movie.CHILDRENS;
+   }
+}
---
+++ RegularPrice.java
@@ -0,0 +1,11 @@
+class RegularPrice extends Price {
+   double getCharge(int daysRented) {
+      double result = 2;
+      if (daysRented > 2)
+         result += (daysRented - 2) * 1.5;
+      return result;
+   }
+    int getPriceCode() {
+       return Movie.REGULAR;
+   }
+}
